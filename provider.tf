4d6272fe6d901b0fc5e71affcfe10f29eb580fee
jaya369 <jayaram886@gmail.com>
2025-02-18 11:31:14 +0530

    vpc module testing files

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+  backend "s3" {
+    bucket = "altodevops-tf-remote-state"
+    key = "vpc-test"
+    region = "us-east-1"
+    dynamodb_table = "altodevops-state-locking"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
